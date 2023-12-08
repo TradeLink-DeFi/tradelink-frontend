2c34dd64582378d75c31b64b987b854951835a95
ribbinpo <ribbinpo@gmail.com>
2023-12-08 16:10:44 +0700

    feat: :sparkles: add offer post nft and token card

--- src/components/OfferPost/OfferPostModule.tsx
+++ src/components/OfferPost/OfferPostModule.tsx
@@ -17,6 +17,7 @@ import { getChains } from "@/services/chain.service";
 import { getNftCollection } from "@/services/nftCollection.service";
 import { getOffers } from "@/services/offer.service";
 import OfferPost from "./OfferPost";
+import { OfferStatus } from "@/interfaces/offer.interface";
 
 export default function OfferPostModule() {
   const { isConnected } = useAccount();
@@ -27,7 +28,12 @@ export default function OfferPostModule() {
   const { data: offerPosts, isLoading } = useQuery({
     queryKey: [
       "offers",
-      { chainId: chainSelected, nftCollectionId: nftSelected, search: search },
+      {
+        chainId: chainSelected,
+        nftCollectionId: nftSelected,
+        search: search,
+        status: OfferStatus.CREATE_OFFER_A,
+      },
     ],
     queryFn: ({ queryKey }) =>
       getOffers(
@@ -35,6 +41,7 @@ export default function OfferPostModule() {
           chainId?: string;
           nftCollectionId?: string;
           search?: string;
+          status?: OfferStatus;
         }
       ),
   });
@@ -81,7 +88,7 @@ export default function OfferPostModule() {
   }, [chainSelected]);
 
   return (
-    <div className="space-y-3">
+    <div className="space-y-3 h-full">
       <div className="flex justify-between items-center">
         <h1 className="text-xl font-semibold">All Offers</h1>
         <Button
@@ -165,12 +172,14 @@ export default function OfferPostModule() {
             >
               {(chain) => (
                 <SelectItem key={chain.value} value={chain.value}>
+                  <div className="flex gap-2">
                   <Image
                     src={`/chains/${chain.label}.png`}
                     alt={chain.label}
                     width={30}
                     height={30}
                   />
+                  </div>
                 </SelectItem>
               )}
             </Select>
@@ -179,10 +188,19 @@ export default function OfferPostModule() {
       </div>
       {isLoading ? (
         <Spinner size="lg" />
-      ) : (
+      ) : offerPosts && offerPosts.length > 0 ? (
         offerPosts?.map((offerPost, key) => (
           <OfferPost key={key} data={offerPost} />
         ))
+      ) : (
+        <div className="text-[#313235] text-center h-full flex flex-col justify-center items-center space-y-6">
+          <Image src="/offers/notFound.svg" alt="not found offer" />
+          <div className="text-lg font-semibold">No offer was found.</div>
+          <div>
+            No offers information was found for a moment, Please <br />
+            come back later to check for new offers.
+          </div>
+        </div>
       )}
     </div>
   );
